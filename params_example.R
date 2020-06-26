4d063519701503232f5843824e37e341515dbb11
Alexandre Rebillard <haldrex.mail@gmail.com>
2020-06-26 08:21:57 -0400

    fixing messy last commit

---
+++ params_example.R
@@ -0,0 +1,13 @@
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# ! This file must be renamed to 'params.R' or sourced file in fsw.R should be change !
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+# fsw params
+fcsDirPath <- "/path/to/fcs/directory"
+referenceFcsFileName <- "example.fcs"
+
+phmDirName <- "phm" # generated inside fsc directory
+extraDirName <- "extra" # generated inside fsc directory
+
+marker_cols <- (7:19)
+nClus <- 21
